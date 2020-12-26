b88428b0d492dab02dc5cb3367936c59f70ebf81
hemanth407 <49086027+hemanth407@users.noreply.github.com>
2020-12-26 21:39:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TabView from "../src/view/TabView/TabView";
+
+function App() {
+  return (
+    <div className="App">
+      <TabView/>
+    </div>
+  );
+}
+
+export default App;
